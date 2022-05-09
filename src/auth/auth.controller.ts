3d201921062078c23cead214cf1f6b1497cdd240
Divyansh Singh <divyanshanandsingh@gmail.com>
2022-05-10 04:06:45 +0530

    switched to cookie based auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,8 +4,10 @@ import {
   HttpCode,
   HttpStatus,
   Post,
+  Res,
   UseGuards,
 } from '@nestjs/common';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { GetCurrentUser, Public } from './decorators';
 import { RegistrationDto, LoginDto } from './dto/index';
@@ -19,15 +21,43 @@ export class AuthController {
   @Public()
   @Post('register')
   @HttpCode(HttpStatus.CREATED)
-  async register(@Body() body: RegistrationDto): Promise<Tokens> {
-    return this.authService.register(body);
+  async register(
+    @Body() body: RegistrationDto,
+    @Res({ passthrough: true }) response: Response,
+  ): Promise<Tokens> {
+    const tokens = await this.authService.register(body);
+    response.cookie('refreshToken', tokens.refreshToken, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 60 * 24 * 7,
+      secure: false,
+    });
+    response.cookie('accessToken', tokens.accessToken, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 60 * 24 * 7,
+      secure: false,
+    });
+    return tokens;
   }
 
   @Public()
   @Post('login')
   @HttpCode(HttpStatus.OK)
-  async login(@Body() body: LoginDto) {
-    return this.authService.login(body);
+  async login(
+    @Body() body: LoginDto,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const tokens = await this.authService.login(body);
+    response.cookie('refreshToken', tokens.refreshToken, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 60 * 24 * 7,
+      secure: false,
+    });
+    response.cookie('accessToken', tokens.accessToken, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 60 * 24 * 7,
+      secure: false,
+    });
+    return tokens;
   }
 
   @Post('logout')
--- src/auth/strategies/refresh.strategy.ts
+++ src/auth/strategies/refresh.strategy.ts
@@ -7,14 +7,18 @@ import { Injectable } from '@nestjs/common';
 export class RefreshStrategy extends PassportStrategy(Strategy, 'rt-jwt') {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request) => {
+          return req.cookies.refreshToken;
+        },
+      ]),
       secretOrKey: 'refresh-secret',
       passReqToCallback: true,
     });
   }
 
   async validate(req: Request, payload: any) {
-    const refreshToken = req.get('Authorization').replace('Bearer ', '').trim();
+    const refreshToken = req.cookies.refreshToken;
     return {
       ...payload,
       refreshToken,
